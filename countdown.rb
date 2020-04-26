2af574eaef22bd94522cd394e55b0b35bbe4fc23
Jessica Gamez <jpgamez1789@icloud.com>
2020-04-26 18:46:00 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,9 @@
 # number = 1
 
-def countdown(number)
+def countdown(int)
   
-  while number > 0
-   puts "#{number} SECOND(S) !"
+  while int > 0
+   puts "#{int} SECOND(S)!"
    number -= 1
   end
   "HAPPY NEW YEAR!"  
